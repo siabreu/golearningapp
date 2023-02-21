8796a402b7856e768aa5eef2ff0230b5deef18a3
Simone de Abreu <siabreu@gmail.com>
2023-02-21 19:17:47 -0300

    adds home page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,8 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# Add the webpack output path to the assets path
+Rails.application.config.assets.paths += [
+  Rails.root.join("public", "dist" ).to_s
+]
