8fa953e52970b683023b1a56b60bee0380010b49
Simone de Abreu <siabreu@gmail.com>
2023-02-27 17:11:58 -0300

    adds scope and active method to course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,11 @@
 class Course < ApplicationRecord
   has_many :enrollments, dependent: :destroy
   has_many :students, through: :enrollments
+
+  # scope
+  scope :active, -> { where(" ? BETWEEN starts_at AND ends_at", Time.now) }
+
+  def active?
+    starts_at < Time.now && Time.now <= ends_at
+  end
 end
