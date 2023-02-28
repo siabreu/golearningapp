ebd14fb65148b5a56df9ad3a9d9414fb6f46aa1d
Simone de Abreu <siabreu@gmail.com>
2023-02-28 00:59:51 -0300

    changes route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :students
-  get "enrollments/create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :courses
   resources :students
-  resources :enrollments, only: [:new, :create]
+  get "/enrollments", to: "enrollments#create"
 
   # Defines the root path route ("/")
   root to: "home#index"
